860c3ab546a7dfbd925cc8aedec22bbd0bc706bf
Lmarcello7 <101017486+Lmarcello7@users.noreply.github.com>
2023-01-03 21:18:25 -0300

    adcionando anotacçoes

--- classes/cliente.php
+++ classes/cliente.php
@@ -1,5 +1,14 @@
 <?php 
 
+//DECLARAÇÃO DE CLASSES #F00
+//Declaração de Classes: Declaração de classes é basicamente um modelo para gerar itens de uma característica específica, 
+//podem existir inúmeras classes dentro de um mesmo arquivo, porém é recomendado criar um arquivo para cada tipo de declaração.
+
+//PROPRIEDADES: #F00
+// Propriedades: São as propriedades que definem uma classe.
+
+// Para definir uma propriedade, é primeiro necessário definir um operador de visibilidade: public, private
+
 class Cliente
 {
     public $nome;
